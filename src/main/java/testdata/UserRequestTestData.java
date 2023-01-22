f4542489b2da53f80a314e31f058e31766a704dc
emmakurganova <emmachuyako@yandex.ru>
2023-01-23 01:21:36 +0300

    Diplom-3 | исправил замечания

--- src/main/java/testdata/UserRequestTestData.java
+++ src/main/java/testdata/UserRequestTestData.java
@@ -8,7 +8,6 @@ public class UserRequestTestData {
     @Step("Запрос на регистрацию пользователя со всеми корректными данными")
     public static UserRequest getUserRequestWithCorrectData() {
         String name = RandomStringUtils.randomAlphabetic(11);
-//        String email = "ilyaK123456789@gmail.com";
         String email = RandomStringUtils.randomAlphanumeric(9) + "@gmail.com";
         String password = RandomStringUtils.randomAlphabetic(7);
         return new UserRequest(name, email, password);
